eb56bffa1673f7eb8c12540b217a165c83dd973f
Michael Parlee <michael.parlee@dexcom.com>
2019-12-03 13:06:15 -0800

    simplifies scripts.

--- hooks/scala-fmt.sh
+++ hooks/scala-fmt.sh
@@ -1,14 +1,4 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/env sh
 
-output="$(scalafmt "$@")"
-
-[[ -z "$output" ]] && exit 0
-
-echo >&2 "Scala files were modified to match formatting rules:"
-for f in $output; do
-  echo >&2 "  $PWD/$f"
-done
-
-exit 1
+scalafmt "$@"
 
