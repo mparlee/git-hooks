e8b7bf4fe260fa60bf12c2c568e4eaad071069b6
Michael Parlee <michael.parlee@dexcom.com>
2020-03-17 11:54:32 -0700

    traps early exit from ktlint command.

--- hooks/ktlint.sh
+++ hooks/ktlint.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-output="$(ktlint -F "$@")"
+function run_ktlint () {
+  output=$(ktlint -F "$@")
+  echo "$output"
+}
+
+output=$(trap run_ktlint EXIT)
 
 [[ -z "$output" ]] && exit 0
 
-echo >&2 "Kotlin files were changed to match formatting rules:"
-for f in $output; do
-  echo >&2 "  $PWD/$f"
-done
+if [[ $output == *"cannot be auto-corrected"* ]]; then
+  echo "$output"
+else
+  echo >&2 "Kotlin files were changed to match formatting rules:"
+  for f in $output; do
+    echo >&2 "  $PWD/$f"
+  done
+fi
 
 exit 1
 
